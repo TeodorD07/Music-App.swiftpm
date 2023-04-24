6170990c58d1fbbba1a608b53fc69ab03968af14
Valerie Morales <vmorales4297@stu.d214.org>
2023-04-24 13:21:58 -0500

    hello

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Text("hello")
+                .foregroundColor(.white)
+                .font(.largeTitle)
+            
         }
+        .background(.blue)
     }
 }
