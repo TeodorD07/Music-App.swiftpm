bb42a0f190308c048645f827be89e0d5ced4cb22
Tanya Cervantes <tcervantes3491@stu.d214.org>
2023-04-26 14:34:43 -0500

    Added custom color and started home page

--- ContentView.swift
+++ ContentView.swift
@@ -4,12 +4,65 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("hello")
+            Text("App Name")
                 .foregroundColor(.white)
                 .font(.largeTitle)
             
+            .padding(10)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Songs")
+                    .foregroundColor(.white)
+                    
+            })
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+
+            .padding(10)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Albums")
+                    .foregroundColor(.white)
+            })
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+
+            .padding(10)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Search")
+                    .foregroundColor(.white)
+            })
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+
+            .padding(10)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Shazam")
+                    .foregroundColor(.white)
+            })
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+           
+            
         }
-        .background(.blue)
-        .opacity(0.2)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(CustomColor.pale)
+        
+
     }
+        
+}
+
+struct CustomColor {
+    static let pale = Color("pale")
 }
