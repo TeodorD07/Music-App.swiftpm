a50e88082b5d86ab1bb1d12bd77bc8943da7b87b
Valerie Morales <vmorales4297@stu.d214.org>
2023-04-28 12:45:13 -0500

    made array

--- SongLibrary.swift
+++ SongLibrary.swift
@@ -1,12 +1,17 @@
 import SwiftUI
 
 struct SongLibrary: View {
-    @State var artistArray : [Library] = []
+    
+//    @State var artistArray : [Library] = [Library(artist: "Lana Del Rey", album: "", song: "")]
+//
+    @State var artists = ["Lana Del Rey", "Kali Uchis"]
+    
+    
     var body: some View {
         VStack {
             
-            List(){
-                Button("artist") {
+            ForEach(0..<artists.count, id: \.self) { array in
+                Button(artists[array]) {
                     
                 }
             }
