e9e29784df36835a5c7fe7118b305b220e83572c
Valerie Morales <vmorales4297@stu.d214.org>
2023-05-26 13:27:53 -0500

    made the homepage buttons turn into the same views in the tab view

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var tabSelection = 0
     var body: some View {
         
 
@@ -9,25 +9,61 @@ struct ContentView: View {
             VStack {
            
                 
-                TabView{
+                TabView(selection: $tabSelection){
+                    VStack {
+                        Text("Shazamify")
+                        
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            .background(Rectangle())
+                            .foregroundColor(CustomColor.mauve)
+                            .padding(10)
+                        
+                       
+                        Button {
+                            tabSelection = 1
+                        } label: {
+                            Text("Search")
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(CustomColor.mauve)
+                        
+                        .padding(10)
+                        Button {
+                            tabSelection = 2
+                        } label: {
+                            Text("Shazam")
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(CustomColor.mauve)
+                        
+                        .padding(10)
+                    }
                     
-                    HomePage()
+                   
                         .tabItem {
                             Label("Home", systemImage: "house")
                         }
+                        .tag(0)
                     
                     WKSearchView()
+        
                         .tabItem {
                             Label("Artists", systemImage: "star")
                         }
+                        .tag(1)
                     
                     ShazamifyView()
                         .tabItem {
                             Label("Songs", systemImage: "heart")
                         }
+                        .tag(2)
                 }
                 
+                
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(CustomColor.pale)
             
             
             
