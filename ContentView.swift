3b16e7567f735a13490ed52828693c7fb5499ddd
Teodor Djuric <tdjuric5081@stu.d214.org>
2023-06-01 12:43:44 -0500

    *Fixed some bugs by returning to older version Added a History View for the Shazam that has not been connected to the app yet

--- ContentView.swift
+++ ContentView.swift
@@ -2,9 +2,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tabSelection = 0
-    @State var tap = false
     var body: some View {
+
+
+
             VStack {
+
+
 
                 TabView(selection: $tabSelection){
                     VStack {
@@ -24,14 +28,6 @@ struct ContentView: View {
                         }
                         .buttonStyle(.borderedProminent)
                         .tint(CustomColor.mauve)
-//                        .scaleEffect(tap ? 1 : 1.2)
-//                                .animation(.spring(response: 0.4, dampingFraction: 0.6))
-//                                .onTapGesture {
-//                                    tap = true
-//                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-//                                        tap = false
-//                                    }
-                                }
                         
                         .padding(10)
                         Button {
@@ -41,28 +37,28 @@ struct ContentView: View {
                         }
                         .buttonStyle(.borderedProminent)
                         .tint(CustomColor.mauve)
-                        
+
                         .padding(10)
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(CustomColor.pale)
-                    
-                   
+
+
                         .tabItem {
                             Label("Home", systemImage: "house")
                         }
                         .tag(0)
-                    
-                    
+
+
                     WKSearchView()
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
                         .background(CustomColor.pale)
-        
+
                         .tabItem {
                             Label("Artists", systemImage: "star")
                         }
                         .tag(1)
-                    
+
                     ShazamifyView()
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
                         .background(CustomColor.pale)
@@ -71,19 +67,18 @@ struct ContentView: View {
                         }
                         .tag(2)
                 }
-                
-                
+
+
             }
-            
-            
-            
-            
+
+
+
         }
         
-
-
+    }
 struct CustomColor {
     static let pale = Color("pale")
     
     static let mauve = Color("mauve")
 }
+
